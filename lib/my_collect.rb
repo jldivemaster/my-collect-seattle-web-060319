a39400636963a742864876729dea5619fa5e1b4e
Jason Leach <jlspursfan@gmail.com>
2019-05-18 17:03:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,10 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    yield(array[i])
-    new_array << array[i]
+    new_array << yield(array[i])
+    #new_array << array[i]
     i += 1
   end
   new_array
 end
 
-my_collect(array[i]) {array[i].upcase!}
